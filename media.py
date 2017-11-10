5fe16aa58f8092a8d242f5ee7c5f18cf9420643b
Himanshu Singh Kushwah <32425447+david-singh@users.noreply.github.com>
2017-11-11 01:04:20 +0530

    Update media.py

--- media.py
+++ media.py
@@ -1,7 +1,7 @@
 class Movie():
+    """ This Class provides a way to store Movie related information"""
 
-     """ This Class provides a way to store Movie related information"""
-
+    
     def __init__(self,movie_title, storyline,poster_image,trailer_youtube_url):     #constructor  or initializer with arg details of Movie
         self.title=movie_title                  
         self.storyline=storyline
