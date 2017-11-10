c65bea7df4040748ec9e7c5b4bb5ba5a056110ac
Himanshu Singh Kushwah <32425447+david-singh@users.noreply.github.com>
2017-11-11 00:46:04 +0530

    Add files via upload

---
+++ media.py
@@ -0,0 +1,13 @@
+class Movie():
+
+     """ This Class provides a way to store Movie related information"""
+
+    def __init__(self,movie_title, storyline,poster_image,trailer_youtube_url):     #constructor  or initializer with arg details of Movie
+        self.title=movie_title                  
+        self.storyline=storyline
+        self.poster_image_url=poster_image
+        self.trailer_youtube_url=trailer_youtube_url
+    
+    
+
+        
